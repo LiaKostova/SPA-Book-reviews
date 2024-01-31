255a600379f311f4e5b44a04087e6cc11c44ce8f
LiaKostova <123201316+LiaKostova@users.noreply.github.com>
2024-01-31 23:42:48 +0200

    My-books-view

---
+++ project/src/views/mybooks.js
@@ -0,0 +1,29 @@
+import { getUserData } from "../utils.js";
+import {html} from '../../node_modules/lit-html/lit-html.js';
+import { getMyBooks } from "../api/data.js";
+
+let mybooksTemplate = (books) => html`
+  <section class="my-books">
+    <div class="my-books-card card">
+      <h1 class="my-books-title">My Books</h1>
+
+      <ul class="my-books-ul" role="list">
+        <li class="my-books-li">
+        ${books.length == 0
+        ? html` <p class="no-books">Add the first book!</p>`
+        : html` 
+            <ul class="mybooks-ul" role="list">
+            ${books.map(bookPreview)}`
+        }
+        </li>
+      </ul>   
+    </div>
+  </section>
+`;
+
+export async function showMyBooks(ctx){
+    let userData = getUserData();
+    let books = await getMyBooks(userData.id);
+
+    ctx.render(mybooksTemplate(books));
+}
